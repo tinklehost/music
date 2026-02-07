ae13ae3cb082ee662a629190314f75eebe84d2f6
tinklehost <strangerthingfanman@gmail.com>
2026-02-07 15:46:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  const productionBase = env.VITE_BASE_PATH || '/aura-music/';
+  const productionBase = env.VITE_BASE_PATH || '/music/';
   return {
     base: mode === 'production' ? productionBase : '/',
     server: {
